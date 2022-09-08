3bc4d5c82bbd6b41defd7db8d20c0f714417ba53
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-09-08 12:09:35 -0700

    Create Button.js
    reusable button component with styling on press

---
+++ components/Button.js
@@ -0,0 +1,33 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+const Button = ({children, onPress}) => {
+    return (
+        <View style = {styles.btnContainer}>
+            <Pressable style = {({pressed}) => pressed && styles.btnPressed} onPress={onPress}>
+                <Text style = {styles.btnText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    btnContainer: {
+        backgroundColor: 'hsl(231, 100%, 19%)',
+        alignSelf: 'center',
+        borderRadius: 10000
+    },
+
+    btnPressed: {
+        backgroundColor: 'hsl(230, 100%, 26%)',
+        borderRadius: 10000
+    },
+
+    btnText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 16,
+        padding: 14,
+    }
+});
+
+export default Button;
