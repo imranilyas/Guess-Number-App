387cce465fa653edfa047553c9c2a7aa45434ce6
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-09-08 15:21:16 -0700

    Update App.js
    keyboard can be dismissed from anywhere on the screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, ImageBackground, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ImageBackground, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartScreen from './screens/StartScreen';
 
 export default function App() {
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <LinearGradient style={styles.container} colors={['hsl(231, 100%, 19%)', '#00d9e4']}>
       <ImageBackground source={require('./assets/images/glitter.jpg')} style={styles.container} imageStyle = {styles.backgroundImg}>
         <SafeAreaView style={[styles.container, styles.space]}>
@@ -13,6 +14,7 @@ export default function App() {
         </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
 // 00e4be
