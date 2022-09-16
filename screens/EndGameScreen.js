b7e9b5a3bc25bc3ed9c2aae4078e0af19c8b9527
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-09-16 11:51:42 -0700

    img for end screen and styling

--- screens/EndGameScreen.js
+++ screens/EndGameScreen.js
@@ -1,20 +1,62 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
+import Button from "../components/Button";
+import Title from "../components/Title";
 
-const EndGameScreen = () => {
+const EndGameScreen = ({myNumber, guessHandlerProp}) => {
     return (
-        <View>
-            <Text>Title: EndGameScreen</Text>
-            <View>
+        <View style={styles.container}>
+            <Title>Game Over</Title>
+            <View style={styles.imgContainer}>
                 {/* Contains the congratulations image */}
+                <Image style={styles.img} source={require('../assets/images/summit.jpg')}/>
             </View>
-            <Text>Your number is {}. It took {} rounds to guess your number.</Text>
+
+            <Text style={styles.text}>Your number is <Text style={styles.keyText}>{myNumber}</Text>. It took <Text style={styles.keyText}>{"__"}</Text> rounds to guess your number.</Text>
+
+            <View style={styles.btnContainer}>
+                <Button onPress={guessHandlerProp}>Play Again</Button>
+            </View>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
 
+    imgContainer: {
+        marginVertical: 25,
+    },
+
+    img: {
+        height: 200,
+        width: 200,
+        alignSelf: 'center',
+        borderRadius: 200,
+        borderWidth: 2,
+        borderColor: 'hsl(231, 100%, 19%)'
+    },
+    
+    text: {
+        textAlign: 'center',
+        fontSize: 24,
+        color: 'white',
+        padding: 10,
+    },
+
+    keyText: {
+        color: '#2a1963'
+    },
+
+    btnContainer: {
+        flexDirection: 'row',
+        marginVertical: 10,
+        width: 250,
+        alignSelf: 'center',
+        position: 'absolute',
+        bottom: 0,
     }
 });
 
