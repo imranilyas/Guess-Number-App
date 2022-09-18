24e70e3425cb4bd9acc5ec57bc97a076b80ab9c1
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-09-17 22:52:04 -0700

    Update SpecificItem.js
    item specifically for the flatlist in the GameScreen + styling

--- components/SpecificItem.js
+++ components/SpecificItem.js
@@ -1,18 +1,50 @@
 import {StyleSheet, View, Text} from 'react-native';
+import { Octicons } from '@expo/vector-icons'; 
 
 const SpecificItem = ({children, round}) => {
     return(
         <View style={styles.container}>
-            <Text>Round: {round}</Text>
-            <Text>Guess: {children}</Text>
+            <View style = {styles.emptySpace}>
+                <Octicons name="dot-fill" size={30} color="white" />
+            </View>
+            <View style = {styles.textContainer}>
+                <Text style={styles.text}>Round: {round}</Text>
+                <Text style={styles.text}>Guess: {children}</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        marginVertical: 10,
+        backgroundColor: 'hsl(231, 100%, 19%)',
+        borderRadius: 10,
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: 'white',
     },
+
+    emptySpace: {
+        backgroundColor: '#00b3bd',
+        padding: 10,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRightWidth: 1,
+        borderColor: 'white',
+    },
+
+    textContainer: {
+        padding: 10,
+    },
+
+    text: {
+        fontSize: 20,
+        color: 'white',
+    }
 });
 
 export default SpecificItem;
